abbdd373fe155e11ba750bc08f1ab5620660a86d
AndrZakhara <art.pruttov@gmail.com>
2018-09-05 13:28:57 +0300

    add pattern singleton

---
+++ index.js
@@ -0,0 +1,33 @@
+var counterModule = (function () {
+    var instance,
+        counter = 0;
+
+    var getCounter = function () {
+        return counter;
+    };
+
+    var increaseCounter = function () {
+        counter ++;
+    };
+
+    var createInstance = function () {
+        return {
+            getCounter: getCounter,
+            increaseCounter: increaseCounter
+        }
+    };
+
+    return {
+        getInstance: function () {
+            return instance || (instance = createInstance());
+        }
+    }
+})();
+
+console.log(counterModule.getInstance().getCounter());
+counterModule.getInstance().increaseCounter();
+console.log(counterModule.getInstance().getCounter());
+counterModule.getInstance().increaseCounter();
+counterModule.getInstance().increaseCounter();
+console.log(counterModule.getInstance().getCounter());
+
